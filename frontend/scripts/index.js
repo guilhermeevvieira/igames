9651c3827ae852d7df45f5405b973fd9615a2f66
tavaresnicole <tavaresnicole05@gmail.com>
2024-01-29 20:24:30 -0300

    ajustes css

---
+++ frontend/scripts/index.js
@@ -0,0 +1,11 @@
+const cadastros = [{
+    nomeDoJogo: 'pacman',
+    valor: 20,
+    data: '12/01/24',
+    pagamento: 'pix',
+    cliente: 'Luiza'
+}]
+
+function mostrarCadastros() {
+    console.log(cadastros);
+};
